63d501f1143ff9107aa301f5a4a768adcbdceb5f
luzidem <luzidem@gmail.com>
2022-11-05 23:03:26 -0500

    Tipos de datos

---
+++ tipos de datos.py
@@ -0,0 +1,59 @@
+# Cadenas de caracteres -> Class STR
+
+# para saber qué tipo de variables es
+mensaje = 'Esto es un texto de prueba'
+print(type(mensaje))
+
+mensaje = 'A'
+print(type(mensaje))
+
+numero_5='5'
+print(type(numero_5))
+
+
+#Numéricos
+# Enteros -> class int
+numero=5
+print(type(numero))
+
+numero=-5
+print(type(numero))
+
+# flotantes -> class float
+
+numero=0.5
+print(type(numero))
+
+numero=0.5
+print(type(numero))
+
+suma_decimal = 3.1 + 2.5
+print(type(suma_decimal))
+
+division=3/1
+print(type(division))
+
+# complejos -> class complex
+complejo=1+2j
+print(type(complejo))
+
+complejo=1+2j
+print(type(complejo.real))
+
+#un número complejo está compuesto de dos números flotantes
+complejo=1+2j
+print(type(complejo.imag))
+
+# Booleanos -> class bool
+curso_aprobado = True
+dificil=False
+print(type(curso_aprobado))
+print(type(dificil))
+
+suma = 4 +  5 == 10
+print(type(suma))
+print(suma)
+
+suma = 4 +  5 == 9
+print(type(suma))
+print(suma)
