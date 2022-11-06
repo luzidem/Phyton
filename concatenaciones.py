efac2765673854691bbb70ab4e956c9ddaa1bcf1
luzidem <luzidem@gmail.com>
2022-11-05 21:25:41 -0500

    Concatenaciones practica

---
+++ concatenaciones.py
@@ -0,0 +1,9 @@
+print('Esto es un'+ ' ' + 'texto')
+print('Mi nombre es {} {}'.format('Luz','Jaramillo'))
+print('Mi nombre es {1} {0}'.format('Luz','Jaramillo'))
+print('Mi nombre es {0} {1}'.format('Luz','Jaramillo'))
+
+
+print('Bienvenido al curso {0} {1} {2} {3}'.format('Python','Luz','Jaramillo','Altuna'))
+
+print('Bienvenido al curso {0} {1}'.format(input('Ingrese el nombre del curso:'),input('Ingrese su nombre:')))
