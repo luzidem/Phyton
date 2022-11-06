3907be13c673bdd856e272b14b6522935464ad5b
luzidem <luzidem@gmail.com>
2022-11-05 21:16:07 -0500

    cambios

--- print.py
+++ print.py
@@ -1,1 +1,11 @@
-print("Mensaje primero")
+print("Bienvenido", end='\n')
+print('Bienvenido 2')
+
+print("Bienvenido", end=' ')
+print('..continuará')
+
+print('Bienvenido','al curso','de Python',sep='\n',end='\n--Termina el mensaje')
+
+print('\n')
+print('Palabra1','Palabra2','Palabra3',sep=' - ')
+print('\n')
