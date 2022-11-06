82109e676ff9602e647c378e43482ed0bb3e139b
luzidem <luzidem@gmail.com>
2022-11-06 00:03:57 -0500

    Trabajando con tuplas

---
+++ tuplas.py
@@ -0,0 +1,23 @@
+# Trabajando con tuplas
+# a diferencia de las listas que son con corchetes esta es con parentesis
+#No se pueden modificar datos de una tupla
+tupla = (0,1,2,'Hola',True,'Hola')
+print(tupla)
+
+# tupla[1] ='Cambiando'
+
+print(tupla[3])
+print(tupla[1:3])
+
+print('La longitud de la tupla es: ',len(tupla))
+
+print('Contando cuantos Hola hay en la tupa:' , tupla.count('Hola'))
+
+print('Posición del elemento Hola hay en la tupa:' , tupla.index('Hola'))
+
+
+# Eliminando elementos de la tupla
+
+
+
+
