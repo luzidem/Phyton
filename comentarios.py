a0acbdcc2345db255a456a298a3277715f46f950
luzidem <luzidem@gmail.com>
2022-11-05 21:48:31 -0500

    nuevos cambios

---
+++ comentarios.py
@@ -0,0 +1,4 @@
+# Comentarios en el código
+print('hola mundo')
+
+''' este es un comentario largo que puede se rincluso un texto, ideal para cuando quieres colocar resumen de lo que estas haciendo'''
